a04584369133e65d9512b4a30758b34d7ac8e549
Jan Timar <jantimar2@gmail.com>
2025-01-02 11:36:33 +0100

    Add extension for current value subject

---
+++ Utils/Sources/Utils/CurrentValueSubject+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Utils
+//
+//  Created by Jan Timar on 01.01.2025.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+extension CurrentValueSubject {
+    public var binding: Binding<Output> {
+        .init(
+            get: { self.value },
+            set: { self.value = $0 }
+        )
+    }
+}
+
