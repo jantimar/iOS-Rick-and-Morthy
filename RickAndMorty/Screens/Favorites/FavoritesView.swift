3123b9215e1f5d5e4c3774775237dff732a7029d
Jan Timar <jantimar2@gmail.com>
2025-01-02 12:32:55 +0100

    Implement favorites screen

--- RickAndMorty/Screens/Favorites/FavoritesView.swift
+++ RickAndMorty/Screens/Favorites/FavoritesView.swift
@@ -6,7 +6,11 @@
 //
 
 import SwiftUI
+import Models
 import Style
+import Atoms
+import Templates
+import Organisms
 
 struct FavoritesView: View {
 
@@ -14,7 +18,45 @@ struct FavoritesView: View {
     @State var viewModel: FavoritesViewModel
 
     var body: some View {
-        Color.red
-            .navigationTitle("Favorites")
+        ZStack {
+            switch viewModel.characters {
+            case let .failure(text):
+                TextView(text, type: .large)
+                    .foregroundStyle(style.colors.foregroundsTertiary)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            case let .data(charactes),
+                    let .refreshing(charactes):
+                TableView(
+                    items: charactes,
+                    cell: { character, _ in
+                        CharacterCard(
+                            title: character.name ?? "-",
+                            subtitle: character.status?.rawValue,
+                            image: character.image,
+                            icon: .arrowRight,
+                            isFavorite: true,
+                            type: .default
+                        )
+                    },
+                    footer: {
+                        Spacer(minLength: style.offsets.large * 4 + 42)
+                    },
+                    action: { character in
+                        viewModel.router.push(.character(character))
+                    }
+                )
+                .padding(.horizontal, style.offsets.extraLarge)
+
+            case .loading:
+                ActivityIndicator()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            default:
+                Color.clear
+            }
+        }
+        .onAppear(perform: viewModel.update)
+        .navigationTitle("Favorites")
+        .background(style.colors.backgroundsPrimary)
+        .animation(.easeIn, value: viewModel.characters)
     }
 }
--- RickAndMorty/Screens/Favorites/FavoritesViewModel.swift
+++ RickAndMorty/Screens/Favorites/FavoritesViewModel.swift
@@ -8,21 +8,68 @@
 import Foundation
 import Combine
 import RickAndMortyAPIService
+import DatabaseService
+import Models
 
 @Observable
 final class FavoritesViewModel {
 
+    var characters: DataState<[Character]> = .unknown
+    private var charactersValueSubject = CurrentValueSubject<[Character], Never>([])
+
     private let apiService: RickAndMortyAPIServiceCharacterProcotol
+    private let databaseService: DatabaseServiceProtocol?
+    let router: Router
+    private var disposBag = Set<AnyCancellable>()
 
     init(
         apiService: RickAndMortyAPIServiceCharacterProcotol,
+        databaseService: DatabaseServiceProtocol?,
         router: Router
     ) {
         self.apiService = apiService
+        self.databaseService = databaseService
+        self.router = router
         self.setup()
     }
 
     private func setup() {
+        let apiService = self.apiService
 
+        charactersValueSubject
+            .map { $0.map(\.id).sorted() }
+            .removeDuplicates()
+            .filter { !$0.isEmpty }
+            .flatMap { ids in
+                apiService.characters(ids: ids)
+            }
+            .receive(on: RunLoop.main)
+            .sink(
+                receiveCompletion: { _ in },
+                receiveValue: { [weak self] characters in
+                    self?.charactersValueSubject.value = characters
+                }
+            )
+            .store(in: &disposBag)
+
+        charactersValueSubject
+            .map { $0.sorted(by: { $0.id < $1.id }) }
+            .sink(receiveValue: { [weak self] characters in
+                self?.characters = .data(characters)
+            })
+            .store(in: &disposBag)
+    }
+
+    func update() {
+        // Fetch data from favorites DB
+        databaseService?.charactersPublisher()
+            .sink(receiveValue: { [weak self] characters in
+                guard characters.count > 0 else {
+                    self?.characters = .failure("No favorites yet")
+                    return
+                }
+                self?.charactersValueSubject.value = characters
+            })
+            .store(in: &disposBag)
     }
 }
