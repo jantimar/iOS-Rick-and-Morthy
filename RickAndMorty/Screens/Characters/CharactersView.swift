fce7970a18b6165314a52f3c147a408fce691589
Jan Timar <jantimar2@gmail.com>
2025-01-02 12:32:39 +0100

    Implement characters screen

--- RickAndMorty/Screens/Characters/CharactersView.swift
+++ RickAndMorty/Screens/Characters/CharactersView.swift
@@ -6,6 +6,11 @@
 //
 
 import SwiftUI
+import Models
+import Style
+import Atoms
+import Templates
+import Organisms
 
 struct CharactersView: View {
 
@@ -13,7 +18,63 @@ struct CharactersView: View {
     @State var viewModel: CharactersViewModel
 
     var body: some View {
-        Color.blue
-            .navigationTitle("Characters")
+        ZStack {
+            switch viewModel.characters {
+            case let .failure(text):
+                TextView(text, type: .large)
+                    .foregroundStyle(style.colors.foregroundsTertiary)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            case let .data(charactes),
+                    let .refreshing(charactes):
+                TableView(
+                    items: charactes,
+                    cell: { character, isSearching in
+                        CharacterCard(
+                            title: character.name ?? "-",
+                            subtitle: character.status?.rawValue,
+                            image: character.image,
+                            icon: .arrowRight,
+                            isFavorite: isSearching ? false : viewModel.isFavorite(character: character),
+                            type: isSearching ? .search : .default
+                        )
+                    },
+                    footer: {
+                        if viewModel.characters.isRefreshing {
+                            HStack {
+                                ActivityIndicator()
+                                Spacer()
+                            }
+                            .padding(.horizontal, style.offsets.large * 2)
+                            .padding(.bottom, style.offsets.large * 4)
+                        } else {
+                            Spacer(minLength: style.offsets.large * 4 + 42)
+                                .onAppear(perform: viewModel.fetchNextPage)
+                        }
+                    },
+                    action: { character in
+                        viewModel.router.push(.character(character))
+                    }
+                )
+                .refreshable { viewModel.refresh() }
+                .padding(.horizontal, style.offsets.extraLarge)
+            case .loading:
+                ActivityIndicator()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            default:
+                Color.clear
+            }
+        }
+        .onAppear(perform: viewModel.refreshFavorites)
+        .navigationTitle("Characters")
+        .searchable(
+            text: viewModel.search.binding,
+            placement: .navigationBarDrawer,
+            prompt: "Search character"
+        )
+        .background(style.colors.backgroundsPrimary)
+        .animation(.easeIn, value: viewModel.characters)
     }
 }
+
+extension Character: Identifiable { }
--- RickAndMorty/Screens/Characters/CharactersViewModel.swift
+++ RickAndMorty/Screens/Characters/CharactersViewModel.swift
@@ -8,21 +8,102 @@
 import Foundation
 import Combine
 import RickAndMortyAPIService
+import Models
+import DatabaseService
 
 @Observable
 final class CharactersViewModel {
 
+    var search = CurrentValueSubject<String, Never>("")
+
+    var characters: DataState<[Character]> = .unknown
+
     private let apiService: RickAndMortyAPIServiceCharacterProcotol
+    private let databaseService: DatabaseServiceProtocol?
+    let router: Router
+    private var charactersRequest: AnyCancellable?
+    private var favorites: [Int] = []
+    private var disposBag = Set<AnyCancellable>()
+
+    private var page = 1
+    private var fetchedPages = [Page<Character>]() {
+        didSet {
+            characters = .data(
+                fetchedPages
+                    .map(\.results)
+                    .flatMap { $0 }
+            )
+        }
+    }
 
     init(
         apiService: RickAndMortyAPIServiceCharacterProcotol,
+        databaseService: DatabaseServiceProtocol?,
         router: Router
     ) {
         self.apiService = apiService
+        self.databaseService = databaseService
+        self.router = router
         self.setup()
     }
 
     private func setup() {
+        refresh()
 
+        search
+            .dropFirst()
+            .debounce(for: 0.3, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .sink(receiveValue: { [weak self] query in
+                self?.page = 1
+                self?.fetchNextPage()
+            })
+            .store(in: &disposBag)
+    }
+
+    func refresh() {
+        page = 1
+        fetchedPages = []
+        fetchNextPage()
+    }
+
+    func refreshFavorites() {
+        databaseService?.charactersPublisher()
+            .sink(receiveValue: { [weak self] favorites in
+                self?.favorites = favorites.map(\.id)
+            })
+            .store(in: &disposBag)
+    }
+
+    func fetchNextPage() {
+        if let data = characters.data, data.count > 0 {
+            characters = .refreshing(data)
+        } else {
+            characters = .loading
+        }
+
+        charactersRequest = apiService.characters(search: search.value, page: page)
+            .receive(on: RunLoop.main)
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    if case .failure = completion {
+                        self?.characters = .failure("No characters found")
+                    }
+                },
+                receiveValue: { [weak self] data in
+                    guard let self else { return }
+                    if self.page == 1 {
+                        self.fetchedPages = [data]
+                    } else {
+                        self.fetchedPages.append(data)
+                    }
+
+                    self.page += 1
+                }
+            )
+    }
+
+    func isFavorite(character: Character) -> Bool {
+        favorites.contains(character.id)
     }
 }
